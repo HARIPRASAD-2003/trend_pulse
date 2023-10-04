5ed25c26fd902cba8a50e9f51e98f135078f090f
HARIPRASAD <hari11122003@gmail.com>
2023-10-04 23:34:52 +0530

    login-signup
    Added empty pages for Login and signup.
    modified route paths to navigate
    /login => Login page
    /signup => signup Page

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Home from './components/Home'
 import {MovieContentDetails, SeriesContentDetails} from './components/ContentDetails'
 import { Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 const Routing = () => {
   return (
@@ -9,6 +11,8 @@ const Routing = () => {
         <Route path="/" Component={Home} />
         <Route path="/movies/details/:contentId" Component={MovieContentDetails} />
         <Route path="/series/details/:contentId" Component={SeriesContentDetails} />
+        <Route path="/login" Component={Login}/>
+        <Route path="/signup" Component={Signup}/>
     </Routes>
   )
 }
